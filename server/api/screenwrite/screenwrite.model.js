722652359efb327e1fb18d887bba75717a839f86
Braun Goodson <braungoodson@gmail.com>
2015-01-21 19:35:33 -0500

    added screenwrite attributes

--- server/api/screenwrite/screenwrite.model.js
+++ server/api/screenwrite/screenwrite.model.js
@@ -4,9 +4,12 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var ScreenwriteSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  id: String,
+  body: String,
+  dob: { type: Date, default: Date.now },
+  edits: [{dob:{type:Date,default:Date.now}}],
+  ratings: [{dob:{type:Date,default:Date.now},stars:{type:Number,min:1,max:10}}],
+  publish: Boolean
 });
 
 module.exports = mongoose.model('Screenwrite', ScreenwriteSchema);
